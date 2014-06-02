901d6057f3326d8cf099f5409e735ba4c508baeb
crwebcr <crweb@voila.fr>
2014-06-02 18:04:04 +0200

    Algorithme de fusion preSpecification et specification

---
+++ src/main/java/fr/insee/cspa/sa/service/UtilResource.java
@@ -0,0 +1,40 @@
+package fr.insee.cspa.sa.service;
+
+import java.util.List;
+
+import ec.tstoolkit.information.Information;
+import ec.tstoolkit.information.InformationSet;
+
+public class UtilResource {
+
+	// static class
+	UtilResource() {}
+	
+	/**
+	 * Merge InformationSet
+	 * @param a : specified InformationSet
+	 * @param b : model InformationSet
+	 * @param c : default InformationSet
+	 */
+	static public InformationSet merge(InformationSet a, InformationSet b, InformationSet c) {
+		
+		List<Information<Object>> itemsA = a.select(Object.class);
+		List<Information<Object>> itemsB = b.select(Object.class);
+		Object itemA, itemB, itemC;
+		
+	    for (Information<Object> info : itemsA) {
+			itemA = info.value; itemB = b.search(info.name,Object.class); itemC = (c!=null)?c.search(info.name,Object.class):null;
+			if (itemA instanceof InformationSet && itemB != null && itemB instanceof InformationSet) 
+				a.set(info.name, merge((InformationSet)itemA,(InformationSet)itemB,(InformationSet)itemC));
+			else if (itemB!=null && itemA.equals(itemC) && !itemB.equals(itemC)) a.set(info.name, itemB);
+		}
+		for (Information<Object> info : itemsB) {
+			itemB = info.value; itemA = a.search(info.name,Object.class); itemC = (c!=null)?c.search(info.name,Object.class):null;
+			if (itemA == null) a.add(info.name, b.getSubSet(info.name));
+			else if (itemA instanceof InformationSet && itemB instanceof InformationSet) 
+				a.set(info.name, merge((InformationSet)itemA,(InformationSet)itemB,(InformationSet)itemC));
+			else if (itemA.equals(itemC) && !itemB.equals(itemC)) a.set(info.name, itemB);
+		}	
+		return a;
+	}
+}
